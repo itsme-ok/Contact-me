29131631b0fe3f7ad0a4e942f9e15bd11a372aeb
itsme-ok <84544208+itsme-ok@users.noreply.github.com>
2021-05-21 11:56:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function saveIssue(e) {
   var issueSeverity = document.getElementById('issueSeverityInput').value;
   var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
   var issueId = chance.guid();
-  var issueStatus = 'Open';
+  
 
   var issue = {
     id: issueId,
     description: issueDesc,
     severity: issueSeverity,
     assignedTo: issueAssignedTo,
-    status: issueStatus
+ 
   }
 
   if (localStorage.getItem('issues') == null) {
@@ -71,16 +71,16 @@ function fetchIssues() {
     var desc = issues[i].description;
     var severity = issues[i].severity;
     var assignedTo = issues[i].assignedTo;
-    var status = issues[i].status;
+    
 
     issuesList.innerHTML +=   '<div class="well">'+
                               '<h6> User id: ' + id + '</h6>'+
-                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              
                               '<h3>' + desc + '</h3>'+
                               '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
                               '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
-                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Thanks!</a> '+
-                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Sorry!</a>'+
+                              
+                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
                               '</div>';
   }
 }
